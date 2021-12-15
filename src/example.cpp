6e318576a084501f64f606c21c3c985b743f72d4
cheminacci <65716931+cheminacci@users.noreply.github.com>
2021-12-15 02:06:31 -0500

    Add files via upload

--- src/example.cpp
+++ src/example.cpp
@@ -18,13 +18,27 @@ int main()
 
 	std::cout << "The delta of 56 to 78 is:\t" << scinge::delta(56, 78) << '\n';
 
+	scinge::Matrix<int> basic(25);
 	scinge::Matrix<int> square(100,100);	
 	scinge::Matrix<int> cube(100, 100, 100);
 	scinge::Matrix<int> tesseract(100, 100, 100, 100);
-
-	std::cout << "\nMy matrix contains " << square.elements.capacity() << " elements\n";
+	
+	std::cout << "\nMy vector contains " << basic.elements.capacity() << " elements\n";
+	std::cout << "My matrix contains " << square.elements.capacity() << " elements\n";
 	std::cout << "My cube contains " << cube.elements.capacity() << " elements\n";
-	std::cout << "My tesseract contains " << tesseract.elements.capacity() << " elements\n";
+	std::cout << "My tesseract contains " << tesseract.elements.capacity() << " elements\n\n";
+
+	basic.populate_vector(5,5);
+	basic.print_vector_element(5);
+
+	square.populate_square_matrix(0,0,42);
+	square.print_square_element(0,0);
+
+	cube.populate_cube(1,6,23, 420);
+	cube.print_cube_element(1,6,23);
+
+	tesseract.populate_tesseract(10,50,75,25, 4200);
+	tesseract.print_tesseract_element(10,50,75,25);	
 
 	return 0;
 }
--- src/matrix.hpp
+++ src/matrix.hpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <numeric>
 #include <cstdint>
+#include <iostream>
 
 namespace scinge
 {
@@ -14,7 +15,7 @@ namespace scinge
 		std::uint64_t number_of_w_elements;
 		
 		public:
-		std::vector<T> elements;
+		std::vector<T> elements{0};
 		
 		Matrix()
 		{
@@ -22,17 +23,97 @@ namespace scinge
 		}	
 
 		Matrix(std::uint64_t number_of_x_elements)
-		{ elements.reserve(number_of_x_elements); }
+		{ 
+			elements.reserve(number_of_x_elements);
+	       		Matrix::number_of_x_elements = number_of_x_elements;	
+		}
 
 		Matrix(std::uint64_t number_of_x_elements, std::uint64_t number_of_y_elements)
-		{ elements.reserve(number_of_x_elements * number_of_y_elements); }
+		{
+	       		elements.reserve(number_of_x_elements * number_of_y_elements);
+			Matrix::number_of_x_elements = number_of_x_elements;
+			Matrix::number_of_y_elements = number_of_y_elements;
+	       	}
 		
 		Matrix(std::uint64_t number_of_x_elements, std::uint64_t number_of_y_elements, std::uint64_t number_of_z_elements)
-		{ elements.reserve(number_of_x_elements * number_of_y_elements * number_of_z_elements);	}
+		{ 
+			elements.reserve(number_of_x_elements * number_of_y_elements * number_of_z_elements);	
+			
+			Matrix::number_of_x_elements = number_of_x_elements;
+			Matrix::number_of_y_elements = number_of_y_elements;
+			Matrix::number_of_z_elements = number_of_z_elements;
+		}
 		
 		Matrix(std::uint64_t number_of_x_elements, std::uint64_t number_of_y_elements, std::uint64_t number_of_z_elements, std::uint64_t number_of_w_elements)
-		{ elements.reserve(number_of_x_elements * number_of_y_elements * number_of_z_elements * number_of_w_elements); }
+		{ 
+			elements.reserve(number_of_x_elements * number_of_y_elements * number_of_z_elements * number_of_w_elements); 
+			Matrix::number_of_x_elements = number_of_x_elements;
+			Matrix::number_of_y_elements = number_of_y_elements;
+			Matrix::number_of_z_elements = number_of_z_elements;
+			Matrix::number_of_w_elements = number_of_w_elements;
+		
+		}
 
+
+		void populate_vector(uint64_t x_position, T value)
+		{	
+			Matrix::elements[x_position] = value;
+		}
+
+		void populate_square_matrix(uint64_t x_position, uint64_t y_position, T value)
+		{	
+			uint64_t location = ((y_position * number_of_x_elements) + x_position ); 
+			Matrix::elements[location] = value;
+		}
+
+		void populate_cube(uint64_t x_position, uint64_t y_position, uint64_t z_position, T value)
+		{	
+			uint64_t location = ((y_position * number_of_x_elements) + x_position ) + (number_of_x_elements * number_of_y_elements * z_position);
+			Matrix::elements[location] = value;
+		}
+
+		void populate_tesseract(uint64_t x_position, uint64_t y_position, uint64_t z_position, uint64_t w_position, T value)
+		{	
+			uint64_t location = ((y_position * number_of_x_elements) + x_position ) + (number_of_x_elements * number_of_y_elements * z_position) + \
+					    (number_of_x_elements * number_of_y_elements * number_of_z_elements * w_position);	
+			Matrix::elements[location] = value;
+		}
+
+		
+		void print_vector_element(uint64_t x)
+		{ std::cout << "Element at vector position " << x << " is:\t" << elements[x] <<'\n'; }
+
+		void print_square_element(uint64_t x_position, uint64_t y_position)
+		{	
+			uint64_t location = ((y_position * number_of_x_elements) + x_position ); 
+			std::cout << "The element at coordinate (" 
+				<< x_position << " , " 
+				<< y_position << ") is:\t" 
+				<< elements[location] << '\n';
+		}
+
+
+		void print_cube_element(uint64_t x_position, uint64_t y_position, uint64_t z_position)
+		{	
+			uint64_t location = ((y_position * number_of_x_elements) + x_position ) + (number_of_x_elements * number_of_y_elements * z_position);
+			std::cout << "The element at coordinate (" 
+				<< x_position << " , " 
+				<< y_position << " , " 
+				<< z_position << ") is:\t" 
+				<< elements[location] << '\n';
+		}
+
+		void print_tesseract_element(uint64_t x_position, uint64_t y_position, uint64_t z_position, uint64_t w_position)
+		{	
+			uint64_t location = ((y_position * number_of_x_elements) + x_position ) + (number_of_x_elements * number_of_y_elements * z_position) + \
+					    (number_of_x_elements * number_of_y_elements * number_of_z_elements * w_position);	
+			std::cout << "The element at coordinate (" 
+				<< x_position << " , " 
+				<< y_position << " , " 
+				<< z_position << " , " 
+				<< w_position << ") is:\t" 
+				<< elements[location] << '\n';
+		}
 	};
 
 
